96a421edc718a45ff18b3c490860af4bbd660caf
kevin <bigfisheyoo@gmail.com>
2020-05-11 15:20:18 +0800

    Initial Commit

---
+++ KVWeakObserve/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KVWeakObserve
+//
+//  Created by kevin on 2020/5/11.
+//  Copyright © 2020 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
